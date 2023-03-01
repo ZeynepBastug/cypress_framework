ea8de0f48035cb28a7412d02d4d2489797819926
ZB <zeynepbstg@hotmail.com>
2023-03-01 15:33:20 -0500

    config and package.json updated

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+// this file is accessible for entire framework
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
@@ -6,6 +7,7 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
     // specifying test files location
-    specPattern: 'cypress/integration/examples/*.js'
+    specPattern: 'cypress/integration/examples/*.js',
+    screenshotsFolder: 'cypress/screenshots'
   },
 });
--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,9 +1,14 @@
 // Spec file is equivalent of java class or test file
-
+/// <reference types="Cypress" /> for Visual Studio
 describe('My First Test Suite', function(){
     it('My FirstTest case', function (){
 
-        cy.visit("https://www.google.com/");
+        cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/")
+        cy.get(".search-keyword").type("ca")
+
+        cy.wait(2000)
+        // selenium get hit url in browser, cypress gets acts like findElement of selenium
+        cy.get(".product:visible").should("have.length", 4)
 
     })
 })
